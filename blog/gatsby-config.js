8dcaa8c784fb9750e3231ad85081c43cfe4f9576
Kanahiro <mediterranean1769@gmail.com>
2020-10-24 23:39:35 +0900

    initial

---
+++ blog/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `Labo288`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    // Add typescript stack into webpack
+    `gatsby-plugin-typescript`,
+  ],
+};
---
+++ blog/src/components/layout/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Helmet from 'react-helmet';
+import Header from './header';
+
+interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
+  children: any;
+}
+
+class DefaultLayout extends React.PureComponent<DefaultLayoutProps> {
+  public render() {
+    return (
+      <div>
+        <Helmet
+          title="Gatsby Default Starter"
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        />
+        <Header />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DefaultLayout;
